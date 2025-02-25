acc296223e2891f2528e2660ca618d3c76bd9a36
adithya_raman <adithyaraman76@gmail.com>
2025-02-25 23:45:11 +0000

    Pending changes exported from your codespace

---
+++ functions.hpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <vector>
+#include <ctime> 
+#include <fstream>
+
+using namespace std;
+
+string pickWord();
+
+bool guess_correct();
+
+bool complete();
+
+char GetNextGuess(const vector <char> already_guessed);
+
+bool checkEndConditions();
+
+
+
+
+
+
+
+
